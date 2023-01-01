f340b543a88e95f46b259b4b65a9977237b060c3
james <gyt455398@gmail.com>
2023-01-01 13:37:56 +0800

    評論區

--- js/config.js
+++ js/config.js
@@ -1,1 +1,13 @@
 let _url=`http://localhost:3000`;
+//編輯使用者圖像
+let userHead=document.querySelector('.navbar-nav>.userHead');
+function addUser(){
+    let myname=localStorage.getItem('myname');
+    let str=''
+        str+=`
+          <a href="#"><img class="rounded-pill" 
+            src="https://ui-avatars.com/api/?name=${myname}&color=fff&background=random"width="32px" height="32px">
+          </a>`
+          userHead.innerHTML=str;
+}
+addUser();
